fc11c82377433edebe3ef45baa18d3bb6e82af45
RafaelNo <fake@22342.com>
2021-06-13 12:48:29 +0200

    Basic yahoo finance interface app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,4 +8,33 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/get-price/<ticker>")
+def get_price(ticker):
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    response = requests.get(url)
+    company_info = response.json()
+
+    print(company_info)
+
+    price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+    company_name = company_info['quoteSummary']['result'][0]['price']['longName']
+    exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
+    currency = company_info['quoteSummary']['result'][0]['price']['currency']
+
+    result = {
+        "price": price,
+        "name": company_name,
+        "exchange": exchange,
+        "currency": currency
+    }
+    print(result)
+
+    return Response(json.dumps(result))
+
+
+if __name__ == '__main__':
+    app.run()
+
 
