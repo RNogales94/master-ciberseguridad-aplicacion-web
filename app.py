3062bf8b1f6f68993195fd90b9fed0a308c712b7
RafaelNo <fake@22342.com>
2021-06-13 12:57:10 +0200

    More error control

--- app.py
+++ app.py
@@ -21,20 +21,23 @@ def get_price(ticker):
 
     print(company_info)
 
-    price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
-    company_name = company_info['quoteSummary']['result'][0]['price']['longName']
-    exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
-    currency = company_info['quoteSummary']['result'][0]['price']['currency']
+    try:
+        price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+        company_name = company_info['quoteSummary']['result'][0]['price']['longName']
+        exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
+        currency = company_info['quoteSummary']['result'][0]['price']['currency']
 
-    result = {
-        "price": price,
-        "name": company_name,
-        "exchange": exchange,
-        "currency": currency
-    }
-    print(result)
+        result = {
+            "price": price,
+            "name": company_name,
+            "exchange": exchange,
+            "currency": currency
+        }
+        print(result)
 
-    return Response(json.dumps(result), status=200, mimetype='application/json')
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    except (KeyError, TypeError):
+        return Response({}, status=404, mimetype='application/json')
 
 
 if __name__ == '__main__':
